88c52dd1d6487be358aee7139ed67213d7588acf
sogamaxc-svg <sogamaxc@gmail.com>
2026-05-29 09:23:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -394,7 +394,7 @@ def load_and_audit_v49():
     
     try:
         st.write("CAMINHO:", DATA_PATH)
-        st.write("EXISTE?", os.path.exists(DATA_PATH))
+        st.write("TAMANHO DO ARQUIVO:", os.path.getsize(DATA_PATH))
         xl = pd.ExcelFile(DATA_PATH, engine="openpyxl")
         
         def clean_df(df):
